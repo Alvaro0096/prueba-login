605a94ef18abdd5b7c7d7ded997be46d29260cf8
AlvaroJorge <alv.jor96@gmail.com>
2022-05-23 15:37:44 -0300

    xhr script created

---
+++ xhr.js
@@ -0,0 +1,48 @@
+const getBtn = document.getElementById('getData');
+const postBtn = document.getElementById('postData');
+
+const sendHttpRequest = (method, url, data) => {
+    const promise = new Promise ((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json';
+        if(data){
+            xhr.setRequestHeader('Content-Type', 'application/json');
+        };
+
+        xhr.onload = () => {
+            if(xhr.status >= 400){
+                reject(xhr.response);
+            } else {
+                resolve(xhr.response);
+            }
+        };
+        
+        xhr.onerror = () => {
+            reject('Something went wrong');
+        };
+
+        xhr.send(JSON.stringify(data));
+    });
+    return promise;
+};
+
+const getData = () => {
+    sendHttpRequest('GET', 'https://reqres.in/api/users').then(responseData => {
+        console.log(responseData)
+    });
+};
+
+const sendData = () => {
+    sendHttpRequest('POST', 'https://reqres.in/api/register', {
+        email: 'eve.holt@reqres.in',
+        password: 'pistol'
+    }).then(responseData => {
+        console.log(responseData);
+    }).catch(error => {
+        console.log(error);
+    });
+};
+
+getBtn.addEventListener('click', getData);
+postBtn.addEventListener('click', sendData);
