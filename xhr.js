0064d38a6bdedd4e48e0076a7e07e27c83bcc88a
AlvaroJorge <alv.jor96@gmail.com>
2022-05-24 17:30:47 -0300

    working xhr

--- xhr.js
+++ xhr.js
@@ -1,61 +1,20 @@
 const form = document.getElementById('register-form');
-let username = document.getElementById('username');
-let pass = document.getElementById('password');
 
-let formData = new FormData();
-    formData.append("username", username);
-    formData.append('password', pass); 
-
-
-const sendHttpRequest = (method, url, data) => {
-    const promise = new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open(method, url);
-  
-    //   xhr.responseType = 'json';
-  
-    //   if (data) {
-    //     xhr.setRequestHeader('Content-Type', 'application/json');
-    //   }
-  
-      xhr.onload = () => {
-        if (xhr.status >= 400) {
-          reject(xhr.response);
-        } else {
-          resolve(xhr.response);
-        }
-      };
-  
-      xhr.onerror = () => {
-        reject('Something went wrong!');
-      };
-  
-      xhr.send(formData);
-    });
-    return promise;
-  };
-  
-//   const getData = () => {
-//     sendHttpRequest('GET', 'https://reqres.in/api/users').then(responseData => {
-//       console.log(responseData);
-//     });
-//   };
-  
-const sendData = () => {
-    sendHttpRequest('POST', 'http://localapps.servegame.com/registerandlogin/register.controller.php')
-      .then(responseData => {
-        console.log(responseData);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
-  form.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
     e.preventDefault();
-    sendData();
-});
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
 
+    const data = new FormData();
+        data.append('username', username.value);
+        data.append('password', password.value);
+    
+    const xhr = new XMLHttpRequest();
 
+    xhr.open('POST','http://localapps.servegame.com/registerandlogin/register.controller.php', true);
 
-
+    xhr.onload = (response) => {
+        console.log(JSON.parse(response.currentTarget.response))
+    }
+    xhr.send(data);
+})
